8eac471ce04cd765510ad5d7173182d0ab918c49
Arkadijus <arkadij.arka@gmail.com>
2021-05-28 20:35:50 +0300

    process

--- OS/Process.cpp
+++ OS/Process.cpp
@@ -1,1 +1,141 @@
 #include "Process.h"
+
+#include <algorithm>
+
+static int currentID = 1;
+
+Process::Process(const Process* parent, ProcessState initialState, std::uint8_t priority,
+	const std::string& name)
+{
+	m_ID = currentID++;
+}
+
+void Process::saveRegisters()
+{
+	m_savedRegisters.SF = m_processor->SF;
+	m_savedRegisters.AX = m_processor->AX;
+	m_savedRegisters.BX = m_processor->BX;
+	m_savedRegisters.PTR = m_processor->PTR;
+	m_savedRegisters.IC = m_processor->IC;
+}
+
+void Process::restoreRegisters()
+{
+	m_processor->SF = m_savedRegisters.SF;
+	m_processor->AX = m_savedRegisters.AX;
+	m_processor->BX = m_savedRegisters.BX;
+	m_processor->PTR = m_savedRegisters.PTR;
+	m_processor->IC = m_savedRegisters.IC;
+}
+
+
+void Process::createProcess(std::unique_ptr<Process>&& process, Process* parent)
+{
+	// change unique ptr location
+	ProcessList.push_back(process);
+	parent->m_childProcesses.push_back(process.get());
+}
+
+void Process::destroyProcess(int ID)
+{
+	int toDelete = -1;
+	Process* process;
+	for (int i = 0; i < ProcessList.size(); i++)
+	{
+		if (ProcessList[i]->getID() == ID)
+		{
+			process = ProcessList[i].get();
+			toDelete = i;
+			break;
+		}
+	}
+
+	if (!process)
+		return;
+
+	// delete resources
+	for (Process* proc : process->m_childProcesses)
+		Process::destroyProcess(proc->getID());
+
+	if (process->m_parent)
+	{
+		auto& parentList = process->m_parent->m_childProcesses;
+		std::remove(parentList.begin(), parentList.end(), process);
+	}
+
+	ProcessList.erase(ProcessList.begin() + toDelete);
+}
+
+void Process::stopProcess(const std::string& name)
+{
+}
+
+void Process::activateProcess(const std::string& name)
+{
+}
+
+void StartStopProcess::run()
+{
+	// initialize system resources
+	// supervisor memory
+	// user memory
+	// initialize system processes
+	std::unique_ptr<Process> readFromInterface(new ReadFromInterfaceProcess(this, ProcessState::Ready, 90));
+	Process::createProcess(std::move(readFromInterface), this);
+
+	std::unique_ptr<Process> JCL(new JCLProcess(this, ProcessState::Ready, 90));
+	Process::createProcess(std::move(JCL), this);
+
+	std::unique_ptr<Process> mainProc(new MainProcProcess(this, ProcessState::Ready, 90));
+	Process::createProcess(std::move(mainProc), this);
+
+	std::unique_ptr<Process> interrupt(new InterruptProcess(this, ProcessState::Ready, 90));
+	Process::createProcess(std::move(interrupt), this);
+
+	// block waiting for MOS end
+	// 
+	// request MOS end
+	// 
+	// delete system processes
+	
+	for (Process* process : m_childProcesses)
+		Process::destroyProcess(process->getID());
+
+	// delete system resources
+}
+
+void ReadFromInterfaceProcess::run()
+{
+	// Block for FromInterface resource
+	// Read file
+	// Block for memory resource
+	// copy file into memory
+	// Send TaskInMemory
+
+
+	
+}
+
+void MainProcProcess::run()
+{
+}
+
+void VirtualMachineProcess::run()
+{
+}
+
+void InterruptProcess::run()
+{
+}
+
+void PrintLineProcess::run()
+{
+}
+
+void JobGovernorProcess::run()
+{
+}
+
+void JCLProcess::run()
+{
+}
--- OS/Process.h
+++ OS/Process.h
@@ -3,8 +3,11 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <memory>
 
 #include "Resource.h"
+#include "CPU.h"
+
 
 struct SavedRegisters
 {
@@ -25,31 +28,111 @@ enum class ProcessState
 	ReadyStopped
 };
 
-class CPU;
-
-struct Process
+class Process
 {
-	// Process list pointer (ready/blocked)
-	std::uint32_t ID;
-	std::string name;
-	SavedRegisters savedRegisters;
-	std::uint8_t priority;
-	CPU* processor;
-	ProcessState state;
-	std::vector<Resource> resourceList;
-	Process* father;
-	std::vector<Process*> childProcesses;
-
+public:
+	Process(const Process* parent, ProcessState initialState,
+		std::uint8_t priority, const std::string& name);
 	void saveRegisters();
 	void restoreRegisters();
 
-	static void createProcess(const Process* parent, ProcessState initialState, );
-	static void destroyProcess();
-	static void stopProcess();
-	static void activateProcess();
+	virtual void run() = 0;
+
+	static void createProcess(std::unique_ptr<Process>&& process, Process* parent);
+	static void destroyProcess(int ID);
+	static void stopProcess(const std::string& name);
+	static void activateProcess(const std::string& name);
+
+
+	std::uint32_t getID() const { return m_ID; }
+protected:
+
+	std::uint32_t m_ID;
+	std::string m_name;
+	SavedRegisters m_savedRegisters;
+	std::uint8_t m_priority;
+	CPU* m_processor;
+	ProcessState m_state;
+	std::vector<Resource> m_createdResList;
+	std::vector<Element> m_elementList;
+	// owned elements
+	Process* m_parent;
+	std::vector<Process*> m_childProcesses;
 };
 
+class StartStopProcess : public Process
+{
+public:
+	StartStopProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "StartStop") {}
+	virtual void run() override;
+};
 
+class ReadFromInterfaceProcess : public Process
+{
+public:
+	ReadFromInterfaceProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "ReadFromInterface") {}
+	virtual void run() override;
+};
+
+class JCLProcess : public Process
+{
+public:
+	JCLProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "JCL") {}
+	virtual void run() override;
+};
+
+class MainProcProcess : public Process
+{
+public:
+	MainProcProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "MainProc") {}
+	virtual void run() override;
+};
+
+class VirtualMachineProcess : public Process
+{
+public:
+	VirtualMachineProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "VirtualMachine") {}
+	virtual void run() override;
+};
+
+class InterruptProcess : public Process
+{
+public:
+	InterruptProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "Interrupt") {}
+	virtual void run() override;
+};
+
+class PrintLineProcess : public Process
+{
+public:
+	PrintLineProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "PrintLine") {}
+	virtual void run() override;
+};
+
+class JobGovernorProcess : public Process
+{
+public:
+	JobGovernorProcess(const Process* parent, ProcessState initialState,
+		std::uint8_t priority)
+		: Process(parent, initialState, priority, "JobGovernor") {}
+	virtual void run() override;
+};
+
+std::vector<std::unique_ptr<Process>> ProcessList;
 
 
 
