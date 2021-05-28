25394695da540e774c06788290262ebd0fd7d02c
Dalius Budrys <dalius.b2011@gmail.com>
2021-05-28 20:18:51 +0300

    Resources
    resources

--- OS/Resource.cpp
+++ OS/Resource.cpp
@@ -1,1 +1,59 @@
 #include "Resource.h"
+#include "Process.h"
+Resource::Resource(Process* parentProcess, std::string name)
+{
+	CreateResource(parentProcess, name);
+}
+
+Resource::~Resource()
+{
+	 
+}
+
+int Resource::CreateResource(Process* parentProcess, std::string name)
+{
+	Resource* resource = new Resource(parentProcess, name);
+	resource->waitingProcesses.push_back((std::unique_ptr<Process>)parentProcess);
+	resource->id = currentId;
+	currentId++;
+	resources.push_back(resource);
+	return resource->id;
+}
+
+void Resource::DeleteResource(int id)
+{
+	for (int i = 0; i < resources.size(); i++)
+	{
+		if (resources[i]->id == id)
+		{
+			for (int j = 0; j < resources[i]->waitingProcesses.size(); j++)
+				resources[i]->waitingProcesses[j]->activateProcess();
+			for (int j = 0; j < resources[i]->elements.size(); i++)
+				delete resources[i]->elements[j];
+			delete resources[i];
+			break;
+		}
+	}
+}
+
+bool Resource::RequestResource(Process* requestingProcess, std::string name)
+{
+	requestingProcess->stopProcess(); // stop it regardless of if there is such a resource or not
+	for (int i = 0; i < resources.size(); i++)
+	{
+		if (resources[i]->name == name) // the resource we need
+			resources[i]->waitingProcesses.push_back((std::unique_ptr<Process>)requestingProcess);
+	}
+	// resursu paskirstytojas?
+	return true;
+}
+
+void Resource::FreeResource(Element* element, std::string name)
+{
+	for (int i = 0; i < resources.size(); i++)
+	{
+		if (resources[i]->name == name) // resource we need
+			resources[i]->elements.push_back(element);
+	}
+	// resursu paskirsytojas
+}
--- OS/Resource.h
+++ OS/Resource.h
@@ -1,7 +1,45 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <memory>
+
+class Process;
+
+// mos end -> sender->interface receiver->startstop
+// task in supervizor mem -> stringu vektorius sender->read from interface receiver->JCL
+// job in supervizor mem -> uint vectorius -> sender->JCL? sender->JOBGOVERNOR? receiver mainproc, struct uint vectoriu ir vykdymo laikas?
+// interrupt event -> sender VirtualMachine receiver interrupt
+// interrupt governor -> sender interrupt, receiver jobgovernor
+
+struct Element
+{
+	Process* sender = nullptr;
+	Process* receiver = nullptr;
+	std::vector<std::string>* strings = nullptr;
+	std::vector<uint32_t>* words = nullptr;
+	~Element()
+	{
+		delete strings;
+		delete words;
+	}
+};
 
 class Resource
 {
-
+	Process* parentProcess = nullptr;
+	std::vector<std::unique_ptr<Process>> waitingProcesses;
+public:
+	int id;
+	std::string name;
+	std::vector<Element*> elements;
+	Resource(Process* parentProcess, std::string name);
+	~Resource();
+	static int CreateResource(Process*, std::string name);
+	static void DeleteResource(int id);
+	static bool RequestResource(Process* requestingProcess, std::string name);
+	static void FreeResource(Element* element, std::string name);
 };
+
+std::vector<Resource*> resources;
+static int currentId = 1;
 
